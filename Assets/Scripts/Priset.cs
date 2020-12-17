cf932ce865810bc847ab4e29cbf37b86336867cd
molinadejan <molinadejan@naver.com>
2020-12-17 14:30:39 +0900

    Add 2d-extras
    타일맵 사용을 위한 추가

--- Assets/Scripts/Priset.cs
+++ Assets/Scripts/Priset.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 public class Priset : MonoBehaviour
 {
     public float moveAmount;
+    public float moveTime;
     public Ease ease;
 
+    /*
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.P)) Move();
     }
+    */
 
     private void Move()
     {
-        transform.DOMove(transform.position + Vector3.up * moveAmount, 0.5f).SetEase(ease);
+        transform.DOMove(transform.position + Vector3.up * moveAmount, moveTime).SetEase(ease);
     }
 }
