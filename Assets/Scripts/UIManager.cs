54086ca840a5b25ca6fbd7f54ece5b960700b86e
molinadejan <molinadejan@naver.com>
2020-12-17 17:15:40 +0900

    2020-12-17 17:15
    UI 수정 및 UI 기능 추가 중

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject content;
+    public Image[] arrowImages;
+
+    public void AddArrow(Sprite arrow)
+    {
+
+    }
+}
