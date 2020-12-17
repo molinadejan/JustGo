ff5f3d661d5fb0448caa7bf25dab6238c3535e56
molinadejan <molinadejan@naver.com>
2020-12-17 14:21:55 +0900

    Priest Move Script
    프리스트 이동 스크립트 생성

---
+++ Assets/Scripts/Priset.cs
@@ -0,0 +1,18 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class Priset : MonoBehaviour
+{
+    public float moveAmount;
+    public Ease ease;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P)) Move();
+    }
+
+    private void Move()
+    {
+        transform.DOMove(transform.position + Vector3.up * moveAmount, 0.5f).SetEase(ease);
+    }
+}
