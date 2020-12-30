4261e77bd97c4312c051d27a975299a2c53bb5b8
molinadejan <molinadejan@naver.com>
2020-12-30 20:56:09 +0900

    Add Banner Ads
    상단 배너 광고 추가

--- Assets/Scripts/Manager/GoogleManager.Admob.cs
+++ Assets/Scripts/Manager/GoogleManager.Admob.cs
@@ -1,7 +1,4 @@
-﻿/*
-using GoogleMobileAds.Api;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using GoogleMobileAds.Api;
 
 public partial class GoogleManager
 {
@@ -10,88 +7,25 @@ public partial class GoogleManager
     private void Start()
     {
         LoadBannerAd();
-        LoadFrontAd();
-        LoadRewardAd();
-    }
-
-    private void Update()
-    {
-        frontAdBtn.interactable = frontAd.IsLoaded();
-        rewardAdBtn.interactable = rewardAd.IsLoaded();
     }
 
     private AdRequest GetAdRequest()
     {
         return new AdRequest.Builder().AddTestDevice("652D1D85D16F87B2").Build();
+        //return new AdRequest.Builder().Build();
     }
 
     #region  Banner
 
     const string bannerTestID = "ca-app-pub-3940256099942544/6300978111";
-    const string bannerID = "";
+    const string bannerID = "ca-app-pub-8176709912093780/4257197214";
     BannerView bannerAd;
 
     private void LoadBannerAd()
     {
-        bannerAd = new BannerView(isTestMode ? bannerTestID : bannerID, AdSize.SmartBanner, AdPosition.Bottom);
+        bannerAd = new BannerView(isTestMode ? bannerTestID : bannerID, AdSize.SmartBanner, AdPosition.Top);
         bannerAd.LoadAd(GetAdRequest());
-        ToggleBannerAd(false);
-    }
-
-    public void ToggleBannerAd(bool b)
-    {
-        if (b) bannerAd.Show();
-        else bannerAd.Hide();
-    }
-
-    #endregion
-
-    #region  Front
-
-    const string frontTestID = "ca-app-pub-3940256099942544/8691691433";
-    const string frontID = "";
-    InterstitialAd frontAd;
-
-    private void LoadFrontAd()
-    {
-        frontAd = new InterstitialAd(isTestMode ? frontTestID : frontID);
-        frontAd.LoadAd(GetAdRequest());
-        frontAd.OnAdClosed += (sender, e) =>
-        {
-            logText.text = "전면광고 성공";
-        };
-    }
-
-    public void ShowFrontAd()
-    {
-        frontAd.Show();
-        LoadFrontAd();
-    }
-
-    #endregion
-
-    #region  Reward
-
-    const string rewardTestID = "ca-app-pub-3940256099942544/5224354917";
-    const string rewardID = "";
-    RewardedAd rewardAd;
-
-    private void LoadRewardAd()
-    {
-        rewardAd = new RewardedAd(isTestMode ? rewardTestID : rewardID);
-        rewardAd.LoadAd(GetAdRequest());
-        rewardAd.OnUserEarnedReward += (sender, e) =>
-        {
-            logText.text = "리워드 광고 성공";
-        };
-    }
-
-    public void ShowRewardAd()
-    {
-        rewardAd.Show();
-        LoadRewardAd();
     }
 
     #endregion
 }
-*/
